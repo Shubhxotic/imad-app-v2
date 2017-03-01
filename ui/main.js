95ea1e608fbca7971a4a7498376ae7724be683db
Shubhxotic <shubh.mischieveous@gmail.com>
2017-03-02 01:16:59 +0530

    Moving the image

--- ui/main.js
+++ ui/main.js
@@ -1,3 +1,16 @@
 console.log('Loaded!');
 var elem=document.getElementById('upper html');
 elem.innerHTML= "This is Shubham's Website";
+
+var marginLeft=0;
+var image=document.getElementById('img-index');
+function moveright() {
+	// body...
+	marginLeft+=10;
+	image.style.marginLeft= marginLeft+'px';
+}
+image.onclick= function()
+{
+	var interval= setInterval(moveright,100);
+}
+
