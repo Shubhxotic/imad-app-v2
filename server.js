2e0b28bb81ecc3355b653d54d7a42e202118bd1d
Shubham Agarwal <shubh.mischieveous@gmail.com>
2017-03-31 23:26:15 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -86,7 +86,7 @@ function createTemplate(data){
 }
 */
 app.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'index.html'));
+  res.sendFile(path.join(__dirname, 'ui', 'index1.html'));
 });
 
 app.get('/ui/style.css', function (req, res) {
